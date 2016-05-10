6ffa54d735acb3647b23318548bf25f342a170c5
kingsmiler <kingsmiler@163.com>
2016-05-10 19:20:13 +0800

    update sql

---
+++ docs/mysql.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE IF NOT EXISTS `mybatis`
+  CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+
+CREATE USER 'mybatis'@'%'
+  IDENTIFIED BY 'mybatis';
+
+GRANT ALL PRIVILEGES ON mybatis.* TO 'mybatis'@'%';
+
+USE mybatis;
+
+CREATE TABLE pet (
+  name    VARCHAR(20),
+  id      MEDIUMINT NOT NULL AUTO_INCREMENT,
+  owner   VARCHAR(20),
+  species VARCHAR(20),
+  sex     CHAR(1),
+  birth   DATE,
+  death   DATE,
+  PRIMARY KEY (id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE USER (
+  id         MEDIUMINT NOT NULL AUTO_INCREMENT,
+  first_name CHAR(30)  NULL,
+  last_name  CHAR(30)  NULL,
+  PRIMARY KEY (id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+DELIMITER //
+CREATE PROCEDURE read_pet(
+  IN  in_name     VARCHAR(100),
+  OUT out_owner   VARCHAR(100),
+  OUT out_species VARCHAR(100),
+  OUT out_sex     VARCHAR(100),
+  OUT out_birth   DATE,
+  OUT out_death   DATE)
+  BEGIN
+    SELECT
+      owner,
+      species,
+      sex,
+      birth,
+      death
+    INTO out_owner, out_species, out_sex, out_birth, out_death
+    FROM pet
+    WHERE name = in_name;
+  END //
+DELIMITER ;
+
+## The following commands are used to execute the stored procedure:
+## CALL read_pet('Fluffy', @out_owner, @out_species, @out_sex, @out_birth, @out_death);
+##
+## Use the following command to view the stored procedure output:
+## SELECT @out_owner, @out_species, @out_sex, @out_birth, @out_death;
+
+DELIMITER //
+CREATE PROCEDURE read_all_pets()
+  BEGIN
+    SELECT
+      name,
+      owner,
+      species,
+      sex,
+      birth,
+      death
+    FROM pet;
+  END //
+DELIMITER ;
+
+## The following command is used to execute the stored procedure:
+## CALL read_all_pets();
+
+DELIMITER $$
+CREATE FUNCTION get_pet_owner(in_name VARCHAR(200))
+  RETURNS
+    VARCHAR(200)
+  BEGIN
+    DECLARE out_owner VARCHAR(200);
+    SELECT owner
+    INTO out_owner
+    FROM pet
+    WHERE name = in_name;
+    RETURN out_owner;
+  END;
+$$
+##  The following command is used to execute the stored function.
+## SELECT get_pet_owner('Fluffy') AS owner; $$
